77c86ede88fdc0a447e5187b284ff1aef3f95a1f
zakiya adam <zakiyaadan12@gmail.com>
2022-04-04 20:43:51 +0300

    created PostListView class

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
+from re import template
+from winreg import QueryInfoKey
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import(
+     ListView
+)
 
 # Create your views here.
+class PostListView(ListView):
+     template_name = "gallery/post_list.html"
+     queryset = Post.objects.all()
+     context_object_name = 'posts'
