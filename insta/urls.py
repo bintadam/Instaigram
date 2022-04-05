845bfb033334ca4c405b1f29b3c007ac3fde1263
zakiya adam <zakiyaadan12@gmail.com>
2022-04-05 15:51:40 +0300

    added media

--- insta/urls.py
+++ insta/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gallery.urls'))
-]
+    path('', include('gallery.urls')),
+] 
 
