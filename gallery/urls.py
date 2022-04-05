825f68a5287c706741014fcc6225d67de3a765b4
zakiya adam <zakiyaadan12@gmail.com>
2022-04-05 12:44:57 +0300

    catchup

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from .views import(
   PostListView
 )
@@ -9,3 +12,5 @@ urlpatterns =[
     path('', PostListView.as_view(), name='post_list'),
 ]
 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
